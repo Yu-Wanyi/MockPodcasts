15027dc8505de49f9a6a814532b76303905b4d79
WanYi <library9612@gmail.com>
2025-08-05 20:43:19 +0800

    Initial Commit

---
+++ MockPodcasts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MockPodcasts
+//
+//  Created by WanYi Yu on 2025/8/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
